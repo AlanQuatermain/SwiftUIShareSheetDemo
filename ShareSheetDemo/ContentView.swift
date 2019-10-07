a9543f118610ee274626d2ba7bf0a354a9073c51
Jim Dovey <jimdovey@mac.com>
2019-10-07 14:01:00 -0700

    Initial Commit

---
+++ ShareSheetDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShareSheetDemo
+//
+//  Created by Jim Dovey on 10/7/19.
+//  Copyright © 2019 Jim Dovey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
